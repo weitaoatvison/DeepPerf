1f5b6b6a431d81795ce50dcb6d8e3b6e2e7c6f3f
wangcw <wangcw@ga88.(none)>
2017-05-11 11:43:04 +0800

    rm the assembler/kepleras

--- Assembler/KeplerAs/Install_locally.sh
+++
@@ -1,6 +0,0 @@
-perl Makefile.PL
-make
-
-#configure the following variables in .bashrc; then source ~/.bashrc
-#export PERL5LIB=/home/xiuxia/PP2017_artifact/KeplerAs/blib/lib/:$PERL5LIB
-#export PATH=/home/xiuxia/PPoPP2017_artifact/KeplerAs/blib/script:$PATH
